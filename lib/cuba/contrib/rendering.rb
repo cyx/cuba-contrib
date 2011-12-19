11b1bd92960ed4c8ab161b3b0e0afa6035913313
Kevin J. Hanna <kevinjackhanna@gmail.com>
2011-12-19 20:24:57 -0200

    Honor Encoding.default_external instead of using another setting

--- lib/cuba/contrib/rendering.rb
+++ lib/cuba/contrib/rendering.rb
@@ -6,7 +6,6 @@ class Cuba
       app.plugin Cuba::Settings
       app.set :template_engine, "erb"
       app.set :views, File.expand_path("views", Dir.pwd)
-      app.set :encoding, Encoding::UTF_8
     end
 
     def view(template, locals = {})
@@ -14,7 +13,7 @@ class Cuba
     end
 
     def partial(template, locals = {})
-      render("#{settings.views}/#{template}.#{settings.template_engine}", locals, default_encoding: settings.encoding)
+      render("#{settings.views}/#{template}.#{settings.template_engine}", locals, default_encoding: Encoding.default_external)
     end
 
     # Render any type of template file supported by Tilt.
