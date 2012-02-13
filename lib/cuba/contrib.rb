4d47a139320d61b32e2513b19e022f908f9c0d20
Cyril David <cyx.ucron@gmail.com>
2012-02-13 21:14:01 +0800

    Restore Cuba::Settings.

--- lib/cuba/contrib.rb
+++ lib/cuba/contrib.rb
@@ -1,9 +1,10 @@
 class Cuba
   CONTRIB_ROOT = File.expand_path("../../", File.dirname(__FILE__))
 
-  autoload :Prelude,    "cuba/contrib/prelude"
-  autoload :Rendering,  "cuba/contrib/rendering"
-  autoload :Mote,       "cuba/contrib/mote"
+  autoload :Prelude,     "cuba/contrib/prelude"
+  autoload :Rendering,   "cuba/contrib/rendering"
+  autoload :Mote,        "cuba/contrib/mote"
+  autoload :Settings,    "cuba/contrib/settings"
   autoload :TextHelpers, "cuba/contrib/text_helpers"
   autoload :FormHelpers, "cuba/contrib/form_helpers"
 end
