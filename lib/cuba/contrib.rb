de0269f6398621d91b76c76792d9ea92f42cb87c
Cyril David <cyx.ucron@gmail.com>
2012-04-20 11:10:53 +0800

    Drop autoload.

--- lib/cuba/contrib.rb
+++ lib/cuba/contrib.rb
@@ -1,9 +1,9 @@
 class Cuba
   CONTRIB_ROOT = File.expand_path("../../", File.dirname(__FILE__))
 
-  autoload :Prelude,     "cuba/contrib/prelude"
-  autoload :Mote,        "cuba/contrib/mote"
-  autoload :TextHelpers, "cuba/contrib/text_helpers"
-  autoload :FormHelpers, "cuba/contrib/form_helpers"
-  autoload :With       , "cuba/contrib/with"
+  require "cuba/contrib/prelude"
+  require "cuba/contrib/mote"
+  require "cuba/contrib/text_helpers"
+  require "cuba/contrib/form_helpers"
+  require "cuba/contrib/with"
 end
