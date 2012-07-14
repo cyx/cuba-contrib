81b2949ca650d48506d198d66cf35f90c1779e4e
Cyril David <cyx.ucron@gmail.com>
2012-07-14 15:14:05 +0800

    Remove form_helpers from required files.
    This should have been done sooner, but I kind of forgot ^_^".

--- lib/cuba/contrib.rb
+++ lib/cuba/contrib.rb
@@ -4,6 +4,5 @@ class Cuba
   require "cuba/contrib/prelude"
   require "cuba/contrib/mote"
   require "cuba/contrib/text_helpers"
-  require "cuba/contrib/form_helpers"
   require "cuba/contrib/with"
 end
